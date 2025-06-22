4b0145849dce8926f106822e1a2eaf9ccc6cdfb6
Mannat Garg <mannatgarg2006@gmail.com>
2025-06-22 13:39:04 +0530

    Working

--- server/index.js
+++ server/index.js
@@ -1,30 +1,52 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const cors = require('cors');
-
-const connectDB = require('./mongodb/connect');
-const postRoutes = require('./routes/postRoutes');
-const dalleRoutes = require('./routes/dalleRoutes');
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
+import mongoose from "mongoose";
+import generateImageRoute from "./routes/GenerateImage.js";
+import posts from "./routes/Posts.js";
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
-app.use(express.json({ limit: '50mb' }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true })); // for form data
 
-app.use('/api/v1/post', postRoutes);
-app.use('/api/v1/dalle', dalleRoutes);
+app.use("/api/generateImage/", generateImageRoute);
+app.use("/api/post/", posts);
 
-app.get('/', async (req, res) => {
-  res.status(200).json({
-    message: 'Hello from DALL.E!',
+// error handler
+app.use((err, req, res, next) => {
+  const status = err.status || 500;
+  const message = err.message || "Something went wrong";
+  return res.status(status).json({
+    success: false,
+    status,
+    message,
   });
 });
 
+app.get("/", async (req, res) => {
+  res.status(200).json({
+    message: "Hello developers from GFG",
+  });
+});
+
+const connectDB = () => {
+  mongoose.set("strictQuery", true);
+  mongoose
+    .connect(process.env.MONGODB_URL)
+    .then(() => console.log("Connected to Mongo DB"))
+    .catch((err) => {
+      console.error("failed to connect with mongo");
+      console.error(err);
+    });
+};
+
 const startServer = async () => {
   try {
-    connectDB(process.env.MONGODB_URL);
-    app.listen(8080, () => console.log('Server started on port 8080'));
+    connectDB();
+    app.listen(8080, () => console.log("Server started on port 8080"));
   } catch (error) {
     console.log(error);
   }
